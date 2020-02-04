bd530eafd1e79ed53558196c9d58149782d25c62
Shubham Jadhav <shubhamjadhav5678@gmail.com>
2020-02-04 09:32:49 +0530

    Final Website - ready for backend

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
     particles: {
-      number: { value: 80, density: { enable: true, value_area: 800 } },
+      number: { value: 50, density: { enable: true, value_area: 800 } },
       color: { value: "#24e6fc" },
       shape: {
         type: "circle",
